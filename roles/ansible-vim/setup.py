a4bd03e6e527d9e640174be2c55e62471e825167
Carl Bäckström <genzorg@gmail.com>
2021-03-28 22:38:25 +0200

    add ansible-vim role

---
+++ roles/ansible-vim/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='ansible-vim',
+        version='0.0.1',
+        packages=[''],
+        url='https://github.com/carlba/ansible-vim',
+        license='MIT',
+        author='cbackstrom',
+        author_email='genzorg@gmail.com',
+        description=''
+)
